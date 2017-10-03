4c5ac5f68a1b6993d6505eea1ca8a7b4188bb629
Eli Joesph Bradley <elijos03@gmail.com>
2017-10-03 17:25:35 -0500

    Add first-person mode.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Scene, Engine, Vector3, Color4, FreeCamera, HemisphericLight, Mesh } from 'babylonjs';
+import 'mousetrap';
 import './index.css';
 
 const canvas = document.querySelector("#renderCanvas") as HTMLCanvasElement;
@@ -6,7 +7,8 @@ const engine = new Engine(canvas, true);
 
 const game = new (class MyScene {
     scene: Scene;
-    private camera: FreeCamera;
+    private thirdPersonCamera: FreeCamera;
+    private firstPersonCamera: FreeCamera;
     private light: HemisphericLight;
     private sphere: Mesh;
     private ground: Mesh;
@@ -16,20 +18,32 @@ const game = new (class MyScene {
         this.scene = new Scene(engine);
         this.scene.clearColor = new Color4(0, 1, 0);
 
-        this.camera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
-        this.camera.lockedTarget = Vector3.Zero();
-        this.camera.attachControl(canvas, false);
+        this.thirdPersonCamera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
+
+        this.firstPersonCamera = new FreeCamera("camera2", new Vector3(0, 5, -10), this.scene);
+        // this.firstPersonCamera.lockedTarget = Vector3.Zero();
+
+        this.scene.activeCamera = this.thirdPersonCamera;
+        this.thirdPersonCamera.attachControl(canvas, false);
 
         this.light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
         this.light.intensity = .5;
 
         this.sphere = Mesh.CreateSphere("sphere1", 16, 2, this.scene);
+        this.thirdPersonCamera.lockedTarget = this.sphere;
+        this.firstPersonCamera.lockedTarget = this.sphere;
         this.sphere.position.y = 1;
         
         this.ground = Mesh.CreateGround("ground1", 6, 6, 2, this.scene);
 
         this.tValue = 0
 
+        Mousetrap.bind('s', () => {
+            if (this.scene.activeCamera === this.firstPersonCamera)
+                this.scene.activeCamera = this.thirdPersonCamera;
+            else if (this.scene.activeCamera === this.thirdPersonCamera)
+                this.scene.activeCamera = this.firstPersonCamera;
+        })
         setInterval(this.updatePosition.bind(this), 25)
     }
 
@@ -37,13 +51,22 @@ const game = new (class MyScene {
         this.tValue += Math.PI / 100;
         this.tValue = this.tValue % (4 * Math.PI);
 
-        console.log(this.tValue);
+        const currentValue = this.piecewiseFunction(this.tValue);
+        const nextValue = this.piecewiseFunction(this.tValue + Math.PI / 100);
+        const scalarDerivative = nextValue.subtract(currentValue);
+        const unitTangent = scalarDerivative.scale(1 / scalarDerivative.length());
 
-        if (this.tValue <= Math.PI || this.tValue >= 3 * Math.PI) {
-            // this.camera.cameraDirection = new Vector3(Math.cos(this.tValue), 0, Math.sin(this.tValue)).subtract(this.camera.cameraDirection);
-            this.sphere.position = this.firstCircle(this.tValue);
+        console.log(currentValue, nextValue, unitTangent);
+
+        this.sphere.position = currentValue;
+        this.firstPersonCamera.position = currentValue.subtract(unitTangent.scale(5)).add(new Vector3(0, 3, 0));
+    }
+
+    piecewiseFunction(t) {
+        if (t <= Math.PI || t >= 3 * Math.PI) {
+            return this.firstCircle(t);
         } else {
-            this.sphere.position = this.secondCircle(this.tValue);
+            return this.secondCircle(t);
         }
     }
 
