62a70b71025304155b14a76be11a617a600a6920
Eli Joesph Bradley <elijos03@gmail.com>
2017-10-03 16:34:21 -0500

    Add animation, and fix BabylonJS bundling!

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,63 @@
-import { Scene, Engine } from 'babylonjs';
+import { Scene, Engine, Vector3, Color4, FreeCamera, HemisphericLight, Mesh } from 'babylonjs';
 import './index.css';
 
 const canvas = document.querySelector("#renderCanvas") as HTMLCanvasElement;
 const engine = new Engine(canvas, true);
 
-const scene = (() => {
-    const scene = new BABYLON.Scene(engine);
-    scene.clearColor = new BABYLON.Color4(0, 1, 0);
+const game = new (class MyScene {
+    scene: Scene;
+    private camera: FreeCamera;
+    private light: HemisphericLight;
+    private sphere: Mesh;
+    private ground: Mesh;
+    private tValue: number;
 
-    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, false);
-    
-    const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = .5;
+    constructor() {
+        this.scene = new Scene(engine);
+        this.scene.clearColor = new Color4(0, 1, 0);
 
-    const sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-    sphere.position.y = 1;
-    
-    const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+        this.camera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
+        this.camera.lockedTarget = Vector3.Zero();
+        this.camera.attachControl(canvas, false);
 
-    return scene;
+        this.light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
+        this.light.intensity = .5;
+
+        this.sphere = Mesh.CreateSphere("sphere1", 16, 2, this.scene);
+        this.sphere.position.y = 1;
+        
+        this.ground = Mesh.CreateGround("ground1", 6, 6, 2, this.scene);
+
+        this.tValue = 0
+
+        setInterval(this.updatePosition.bind(this), 25)
+    }
+
+    updatePosition() {
+        this.tValue += Math.PI / 100;
+        this.tValue = this.tValue % (4 * Math.PI);
+
+        console.log(this.tValue);
+
+        if (this.tValue <= Math.PI || this.tValue >= 3 * Math.PI) {
+            // this.camera.cameraDirection = new Vector3(Math.cos(this.tValue), 0, Math.sin(this.tValue)).subtract(this.camera.cameraDirection);
+            this.sphere.position = this.firstCircle(this.tValue);
+        } else {
+            this.sphere.position = this.secondCircle(this.tValue);
+        }
+    }
+
+    firstCircle(t) {
+        return new Vector3(Math.cos(t) + 1, 1, Math.sin(t))
+    }
+
+    secondCircle(t) {
+        return new Vector3(-Math.cos(t) - 1, 1, Math.sin(t))
+    }
 })();
 
 // Register a render loop to repeatedly render the scene
-engine.runRenderLoop(scene.render.bind(scene));
+engine.runRenderLoop(game.scene.render.bind(game.scene));
 
 // Watch for browser/canvas resize events
 window.addEventListener("resize", engine.resize.bind(engine));
