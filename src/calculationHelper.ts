1c273f43e775fac3b4ce5ba9cb5f14da382d97f4
Eli Joesph Bradley <elijos03@gmail.com>
2017-10-09 16:25:21 -0500

    Remove decko.

--- src/calculationHelper.ts
+++ src/calculationHelper.ts
@@ -1,5 +1,4 @@
 import { Vector3 } from 'babylonjs';
-import { memoize } from 'decko';
 
 export type FunctionBounds = {
     begin: number,
@@ -16,38 +15,32 @@ export default class CalculationHelper {
         this.bounds = bounds;
     }
 
-    @memoize
     valueAfter(t: number) {
         return (t + this.bounds.increment) % (this.bounds.final - this.bounds.begin) + this.bounds.begin;
     }
 
-    @memoize
     scaledDifferential(t: number) {
         const currentValue = this.pathFunction(t); // r(t)
         const nextValue = this.pathFunction(this.valueAfter(t)); // r(t + ∆t) ≈ r(t + dt)
         return nextValue.subtract(currentValue); // ∆r ≈ r' * ∆t = dr
     }
 
-    @memoize
     unitTangent(t: number) {
         const scaledDifferential = this.scaledDifferential(t);
         return scaledDifferential.scale(1 / scaledDifferential.length()); // r' * ∆t / (||r'|| * ∆t) = r' / ||r'|| = T(t)
     }
 
-    @memoize
     cartRotation(t: number) {
         const unitTangent = this.unitTangent(t);
         return new Vector3(0, -Math.atan2(unitTangent.z, unitTangent.x), -Math.acos(unitTangent.y));
     }
 
-    @memoize
     scaledSecondDifferential(t: number) {
         const scaledDifferential = this.scaledDifferential(t); // ∆r ≈ r' * ∆t = dr
         const nextDifferential = this.scaledDifferential(this.valueAfter(t)); // r'(t + ∆t) * ∆t ≈ dr(t + dt)
         return nextDifferential.subtract(scaledDifferential); // ∆dr ≈ (r' * ∆t)' * ∆t = r'' * ∆t^2 = (d^2)r
     }
 
-    @memoize
     curvature(t: number) {
         const scaledDifferential = this.scaledDifferential(t);
         const secondDifferential = this.scaledSecondDifferential(t);
@@ -55,7 +48,6 @@ export default class CalculationHelper {
         // ||(r' * ∆t) X (r'' * ∆t^2)|| / (||r'||^3 * ∆t^3) = ||r' X r''|| / ||r'||^3 = K(t)
     }
 
-    @memoize
     cameraPosition(t: number) {
         const currentValue = this.pathFunction(t);
         const unitTangent = this.unitTangent(t);
