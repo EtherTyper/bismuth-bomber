c4a03557446f1697e342f57e07cdd594506227ab
Eli Joesph Bradley <elijos03@gmail.com>
2017-10-13 08:00:30 -0500

    Use circles example instead of integrals example.

--- src/pathFunction.ts
+++ src/pathFunction.ts
@@ -3,19 +3,23 @@ import { FunctionBounds } from './calculationHelper';
 import CalculationHelper from './calculationHelper';
 
 export const bounds = {
-    begin: -2,
-    increment: 1 / 100,
-    final: 2
+    begin: 0,
+    increment: Math.PI / 100,
+    final: 6 * Math.PI
 }
 
 export default function pathFunction(t) {
-    function xSquared(t) {
-        return new Vector3(1, t, 1);
+    function firstCircle(t) {
+        return new Vector3(Math.cos(t) + 1, 3 + Math.sin(t), Math.sin(t))
     }
 
-    return CalculationHelper.integrateVVF(xSquared, {
-        begin: 0,
-        final: t,
-        increment: 1 / 100
-    })
+    function secondCircle(t) {
+        return new Vector3(-2 * Math.cos(t / 2 + Math.PI / 2) - 2, 3 + Math.sin(t), 2 * Math.sin(t / 2 + Math.PI / 2))
+    }
+
+    if (t <= Math.PI || t >= 5 * Math.PI) {
+        return firstCircle(t);
+    } else {
+        return secondCircle(t);
+    }
 }
